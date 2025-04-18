b89d73b1e8236418d19c9f79883347e40de093e3
abhishek <abhidubey2129@gmail.com>
2025-04-19 03:10:23 +0530

    first commit

---
+++ src/main/java/com/githubTesting/GithubTestingApplication.java
@@ -0,0 +1,13 @@
+package com.githubTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/githubTesting/GithubTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
