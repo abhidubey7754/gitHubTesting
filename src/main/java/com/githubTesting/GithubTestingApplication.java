e4a399834b8dc618535c1b3485709b8396fd73ca
abhishek <abhidubey2129@gmail.com>
2025-04-19 16:39:43 +0530

    123

--- src/main/java/com/githubTesting/GithubTestingApplication.java
+++ src/main/java/com/githubTesting/GithubTestingApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GithubTestingApplication {
 
+	int z=190;
+        int y=189;
+        String name="abhishek";
+
 	public static void main(String[] args) {
 		SpringApplication.run(GithubTestingApplication.class, args);
 	}
